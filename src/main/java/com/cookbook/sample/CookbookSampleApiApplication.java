6b43f606949c1482ba26cf532f0677e3a4c287cf
mazhar-hassan <netsurgeon@gmail.com>
2019-07-16 19:35:43 +0500

    Inception

---
+++ src/main/java/com/cookbook/sample/CookbookSampleApiApplication.java
@@ -0,0 +1,13 @@
+package com.cookbook.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookbookSampleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookbookSampleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cookbook/sample/CookbookSampleApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cookbook.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CookbookSampleApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
